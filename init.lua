278a0ddf290aba13dc70630dd858e59f002aaaa8
kyle.wilson <kyle.wilson@foxmail.com>
2023-09-09 15:15:53 +0800

    auto tree

--- init.lua
+++ init.lua
@@ -83,5 +83,11 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_create_autocmd("VimEnter", {
+      command = "set nornu nonu | Neotree toggle",
+    })
+    vim.api.nvim_create_autocmd("BufEnter", {
+      command = "set rnu nu",
+    })
   end,
 }
