f454b873afd78ba352bc4b7355343bd62f84e740
kyle.wilson <kyle.wilson@foxmail.com>
2023-09-17 08:22:14 +0800

    lazy load for todo

--- plugins/todo-comments.lua
+++ plugins/todo-comments.lua
@@ -3,7 +3,6 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
-    event = "User AstroFile",
     keys = {
       { "<leader>fT", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
     },
