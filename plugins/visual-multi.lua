53687a4f9836efaca3ac10071076a721fedce3fa
kyle.wilson <kyle.wilson@foxmail.com>
2023-09-19 09:23:12 +0800

    diff

--- plugins/visual-multi.lua
+++ plugins/visual-multi.lua
@@ -3,8 +3,8 @@ return {
     "mg979/vim-visual-multi",
     event = "User AstroFile",
     keys = {
-      { "<leader>sd", ":call vm#commands#add_cursor_down(0, v:count1)<cr>", desc = "move down vertically" },
-      { "<leader>su", ":call vm#commands#add_cursor_up(0, v:count1)<cr>", desc = "move up vertically" },
+      { "<leader>sd", "<cmd>call vm#commands#add_cursor_down(0, v:count1)<cr>", desc = "move down vertically" },
+      { "<leader>su", "<cmd>call vm#commands#add_cursor_up(0, v:count1)<cr>", desc = "move up vertically" },
     },
   },
 }
