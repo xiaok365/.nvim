baa5162c3f0f40a58002ea03bed2e3db4fd85845
kyle.wilson <kyle.wilson@foxmail.com>
2023-11-21 15:56:15 +0800

    bqf&surround

---
+++ plugins/bqf.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "kevinhwang91/nvim-bqf",
+    event = "User AstroFile",
+    opts = {},
+    keys = {
+      { "<leader>im", "<cmd>make<cr>", desc = "make" },
+      { "<leader>io", "<cmd>copen<cr>", desc = "Open qf window" },
+      { "<leader>ic", "<cmd>cclose<cr>", desc = "Close qf window" },
+    },
+  },
+}
